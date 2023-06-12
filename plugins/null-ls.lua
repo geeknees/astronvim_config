85e050563466b30c0fbe1a021a059bb3c6130600
Masumi Kawasaki <geeknees@gmail.com>
2023-06-13 01:39:48 +0900

    feat: Add cspell.json and update plugins
    Create a new cspell.json to allow compound words and add "nvim" to the word list. In plugins/mason.lua, set up new LSP configurations for "tsserver", "solargraph" and "solc", and new Formatters/Linter installations for "prettier", "cspell", "solhint", "eslint_d", and "stylua". In plugins/null-ls.lua, add more sources including cspell, rubocop, and configure the diagnostics_postprocess for cspell and the prefer_local flag for rubocop based on specific file conditions. In plugins/treesitter.lua, add more parsers to ensure_installed list and in plugins/user.lua, add new plugins for Rails, reference, whitespace, matchup, ghpr-blame, vim-test, wakatime, vim-chatgpt, and copilot.

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,17 +1,37 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, config)
+      -- config variable is the default configuration table for the setup function call
+      local null_ls = require "null-ls"
 
-    -- Check supported formatters and linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
-    }
-    return config -- return final config table
-  end,
+      -- Check supported formatters and linters
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+      config.sources = {
+          -- Set a formatter
+          -- null_ls.builtins.formatting.stylua,
+          -- null_ls.builtins.formatting.prettier,
+          null_ls.builtins.code_actions.cspell,
+          null_ls.builtins.diagnostics.cspell.with {
+              diagnostics_postprocess = function(diagnostic)
+                -- レベルをINFOに変更（デフォルトはERROR）
+                diagnostic.severity = vim.diagnostic.severity.INFO
+              end,
+              condition = function()
+                -- cspellが実行できるときのみ有効
+                return vim.fn.executable "cspell" > 0
+              end,
+              extra_args = { "--config", vim.fn.expand "~/.config/nvim/lua/user/cspell.json" },
+          },
+          require("null-ls").builtins.diagnostics.rubocop.with {
+              prefer_local = "bundle_bin",
+              condition = function(utils) return utils.root_has_file { ".rubocop.yml" } end,
+          },
+          require("null-ls").builtins.formatting.rubocop.with {
+              prefer_local = "bundle_bin",
+              condition = function(utils) return utils.root_has_file { ".rubocop.yml" } end,
+          },
+      }
+      return config -- return final config table
+    end,
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,25 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+
+    { "tpope/vim-rails" },
+    { "thinca/vim-ref" },
+    {
+        "ntpeters/vim-better-whitespace",
+    },
+    { "andymass/vim-matchup" },
+    { "rhysd/ghpr-blame.vim" },
+    { "janko-m/vim-test" },
+    -- { "ActivityWatch/aw-watcher-vim" },
+    { "wakatime/vim-wakatime" },
+    { "zalgo3/vim-chatgpt",   branch = "nvim" },
+    { "github/copilot.vim" },
 }
